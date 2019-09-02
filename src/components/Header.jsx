e08a0c59ec3e16cda0b64538f6cfe440e03d36bc
inwisloklem <89099109104@mail.ru>
2019-09-02 22:03:57 +0300

    update: ecommerce

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,39 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Header.module.sass'
 
-function Header () {
+function Header ({ currentUser }) {
+  if (!currentUser) {
+    return null
+  }
+
   return (
-    <nav className={styles.list}>
-      <NavLink className={styles.link} exact to='/'>
-        Home
-      </NavLink>
+    <div className={styles.inner}>
+      <nav className={styles.list}>
+        <NavLink className={styles.link} exact to='/'>
+          Home
+        </NavLink>
 
-      <NavLink className={styles.link} exact to='/shop'>
-        Shop
-      </NavLink>
+        <NavLink className={styles.link} exact to='/shop'>
+          Shop
+        </NavLink>
 
-      <NavLink className={styles.link} exact to='/signin'>
-        Sign In
-      </NavLink>
-    </nav>
+        <NavLink className={styles.link} exact to='/signin'>
+          Sign In
+        </NavLink>
+      </nav>
+
+      <div className={styles.info}>{currentUser.email}</div>
+    </div>
   )
+}
+
+Header.propTypes = {
+  currentUser (props, propName, componentName) {
+    if (typeof props[propName] === 'object') {
+      return
+    }
+    return new Error(`${componentName} only accepts object or null`)
+  }
 }
 
 export default Header
--- src/components/Inner.jsx
+++ src/components/Inner.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Header from './Header'
+import { auth } from '../firebase'
 import Auth from './Auth'
 import Home from './Home'
 import Shop from './Shop'
 import styles from './Inner.module.sass'
 
-function Inner () {
-  return (
-    <main className={styles.component}>
-      <Header />
+class Inner extends Component {
+  constructor () {
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
 
-      <Switch>
-        <Route component={Home} exact path='/' />
-        <Route component={Shop} exact path='/shop' />
-        <Route component={Auth} exact path='/signin' />
-      </Switch>
-    </main>
-  )
+  unsubscribeAuth = null
+
+  componentDidMount () {
+    this.unsubscribeAuth = auth.onAuthStateChanged(user => {
+      const { email } = user
+      this.setState({ currentUser: user })
+      console.info(email)
+    })
+  }
+
+  componentWillUnmount () {
+    this.unsubscribeAuth()
+  }
+
+  render () {
+    const { currentUser } = this.state
+
+    return (
+      <main className={styles.component}>
+        <Header currentUser={currentUser} />
+
+        <Switch>
+          <Route component={Home} exact path='/' />
+          <Route component={Shop} exact path='/shop' />
+          <Route component={Auth} exact path='/signin' />
+        </Switch>
+      </main>
+    )
+  }
 }
 
 export default Inner
